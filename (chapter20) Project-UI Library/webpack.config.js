832a46d1aa83714b4f07853f3c9aaf34f37a6e4f
yaroslav <yarik22k@gmail.com>
2024-11-28 13:18:25 +0100

    0.2

--- (chapter20) Project-UI Library/webpack.config.js
+++ (chapter20) Project-UI Library/webpack.config.js
@@ -1,35 +1,40 @@
-const path = require('path'); 
+const path = require('path');
 
-module.exports = {  
-    entry: './src/index.js', 
-    output: {
-        path: path.resolve(__dirname, 'dist/assets'), 
-        filename: 'bundle.js' 
-    },
-    module: { 
-        rules: [
-            {
-                test: /\.js$/, 
-                exclude: /node_modules/, 
-                use: { 
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'] 
-                    }
-                }
-            },
-            {
-                test: /\.css$/, // Собирает все CSS стили и вгружает в основной файл, но он должне быть ипортриован в index.js потому что вебпак рассматривате его как "точка входа" и там же ищет ВСЕ зависимости
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    devServer: { 
-        static: {
-            directory: path.resolve(__dirname, "dist"), 
-        },
-        devMiddleware: {
-            publicPath: '/assets/', 
+module.exports = {
+  entry: './src/index.js', 
+  output: {
+    path: path.resolve(__dirname, 'dist'),  
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/images/[name][ext][query]', 
+  },
+  module: { 
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        use: { 
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'] 
+          }
         }
+      },
+      {
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,  // Обработка изображений
+        type: 'asset/resource',  // Для Webpack 5
+      },
+    ]
+  },
+  devServer: { 
+    static: {
+      directory: path.resolve(__dirname, "dist"), 
     },
+    devMiddleware: {
+      publicPath: '/assets/', 
+    }
+  },
 };
--- 23/src/index.js
+++ 23/src/index.js
@@ -1,5 +1,14 @@
+// Импорт нормализации стилей
 import 'normalize.css';
-import './img/MainImg1.png';
-import './img/MainImg2.jpg';
+
+// Импорт всех изображений из папки
+const importAllImages = (requireContext) => {
+  requireContext.keys().forEach(requireContext);
+};
+importAllImages(require.context('./img', false, /\.(png|jpe?g|gif|svg)$/));
+
+// Импорт стилей Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Пользовательские стили
 import './styles/style.css';
